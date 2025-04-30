c4a9e2b831a49bc0bd7661c51b5be6edb27cd08d
Setayesh <setayesh2332@gmail.com>
2025-04-29 23:59:40 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import AboutMe from "./components/AboutMe";
+<<<<<<< HEAD
 import Skills from "./components/Skills"; 
+=======
+import Skills from "./components/Skills"; //*here */
+>>>>>>> fe71001 (Update App.tsx)
 import Projects from "./components/Projects";
 import Socials from "./components/Socials";
 import Contact from "./components/Contact";
