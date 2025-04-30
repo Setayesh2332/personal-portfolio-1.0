a04a28dbaf8dfd769d033b60a1102839494bffd6
Setayesh <setayesh2332@gmail.com>
2025-04-30 07:06:51 -0400

    Remove hidden from the form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,10 +2,9 @@ function Contact() {
     return (
       <section id="contact" className="contact-section">
         <h2>Contact</h2>
-        <form name="contact" method="POST" data-netlify="true" className="contact-form">
+        <form name="contact" action="POST" data-netlify="true" className="contact-form">
           <input type="text" name="name" placeholder="Name:" required />
           <input type="email" name="email" placeholder="Email:" required />
-          <input type="hidden" name="form-name" value="contact" />
           <textarea name="message" rows={4} placeholder="Message:" required></textarea>
           <button type="submit" className="cv-btn-mobile">Send</button>
         </form>
