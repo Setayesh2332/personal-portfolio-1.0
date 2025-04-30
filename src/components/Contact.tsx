bf5f620b66ecca97f55858a6346bfc955c172e52
Setayesh <setayesh2332@gmail.com>
2025-04-30 07:19:17 -0400

    Fix: Add Netlify form hidden input

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,6 +3,7 @@ function Contact() {
       <section id="contact" className="contact-section">
         <h2>Contact</h2>
         <form name="contact" method="POST" data-netlify="true" className="contact-form">
+          <input type="hidden" name="form-name" value="contact" />
           <input type="text" name="name" placeholder="Name:" required />
           <input type="email" name="email" placeholder="Email:" required />
           <textarea name="message" rows={4} placeholder="Message:" required></textarea>
